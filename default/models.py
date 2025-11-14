f76a8fab931187c5765b3cc8b1527776b6fd60d4
jerryyang <jerryyang971126@gmail.com>
2025-11-14 12:05:01 +0800

    第一次

--- default/models.py
+++ default/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Poll(models.Model):    #建立群組套用模型
+    subject = models.CharField("投票主題",max_length=64)    #引用模型單行文字取名字
+    desc = models.TextField("說明")  #多行文字
+    created = models.DateField("建立日期",auto_now_add=True)   #第一次建立時間
+
+class Option(models.Model):
+    title = models.CharField("選項文字",max_length=64)
+    votes = models.IntegerField("票數", default=0) #預設值為0
+    poll_id = models.IntegerField("投票主題編號")
