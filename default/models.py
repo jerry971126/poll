1732e43b546cd774425e4aee82fef94e63b30fd8
jerryyang <jerryyang971126@gmail.com>
2025-11-21 11:11:12 +0800

    定義poll,option 資料模型

--- default/models.py
+++ default/models.py
@@ -6,7 +6,14 @@ class Poll(models.Model):    #建立群組套用模型
     desc = models.TextField("說明")  #多行文字
     created = models.DateField("建立日期",auto_now_add=True)   #第一次建立時間
 
+    def __str__(self):      #建立函式
+        return self.subject    #回傳投票資料
+
 class Option(models.Model):
     title = models.CharField("選項文字",max_length=64)
     votes = models.IntegerField("票數", default=0) #預設值為0
     poll_id = models.IntegerField("投票主題編號")
+    
+    def __str__(self):      #建立函式
+        return"{} - {}".format(self.poll_id,self.title)     #替代文字
+        #return f"{self.poll_id}" - {self.title}     #f=fstring 跟上面的一樣
--- poll/settings.py
+++ poll/settings.py
@@ -103,7 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'zh-ppyhant'
+LANGUAGE_CODE = 'zh-hant'
 
 TIME_ZONE = 'Asia/Taipei'
 
