f38735541b9699d42dc9e9aa099f7889f6d6c98c
jerryyang <jerryyang971126@gmail.com>
2025-12-05 11:44:46 +0800

    完成投票主題列表查看投票

--- default/urls.py
+++ default/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
-
+from .views import poll_list,PollList,PollView, PollVote
 urlpatterns =[
+    path("",poll_list),     #poll路徑請求交給後面的處理
+    path("list",PollList.as_view()),
+    path("<int:pk>/",PollView.as_view(),name='poll_view'),
+    path("<int:oid>/vote/",PollVote .as_view(), name ='poll_vote')
     
 ]
--- default/views.py
+++ default/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from .models import Poll,Option     #從models引用
+from django.views.generic import ListView,DetailView , RedirectView
+from django.urls import reverse
 
 # Create your views here.
+def poll_list(req):
+    polls=Poll.objects.all()    #從後面拿取資料給前面的變數
+    return render (req,"default/list.html",{'poll_list':polls,'msg':'Hello'})
+
+class PollList(ListView):
+    model = Poll
+
+    #應用程式名稱/資料模型_list.html
+    #default/poll_list.html
+class PollView(DetailView):
+    model = Poll
+    #default/poll_detail.html
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        ctx['option_list']=option_list = Option.objects.filter(poll_id=self.object.id)
+        return ctx
+class PollVote(RedirectView):
+    def get_redirect_url(self, *args, **kwargs):
+        option = Option.objects.get(id = self.kwargs['oid'])
+        option.votes += 1 #option.votes = options.vote +1
+        option.save()
+        #return "/poll/{}/".format(option.poll_id)
+        #return f"/poll/{option.poll_id}"
+        return reverse("poll_view",args=[option.poll_id])
+        #return reverse("poll_view",kwargs=['pk':option.poll_id])
