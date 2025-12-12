1355d8d752194aa1169010d9f6c8e0b9309ce858
jerryyang <jerryyang971126@gmail.com>
2025-12-12 12:07:52 +0800

    12/12

--- default/urls.py
+++ default/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import poll_list,PollList,PollView, PollVote
+from .views import poll_list,PollList,PollView, PollVote, PollCreate,PollEdit
 urlpatterns =[
-    path("",poll_list),     #poll路徑請求交給後面的處理
-    path("list",PollList.as_view()),
+         #poll路徑請求交給後面的處理
+    path("",PollList.as_view(),name='poll_list'),
     path("<int:pk>/",PollView.as_view(),name='poll_view'),
-    path("<int:oid>/vote/",PollVote .as_view(), name ='poll_vote')
-    
+    path("<int:oid>/vote/",PollVote.as_view(), name ='poll_vote'),
+    path("add",PollCreate.as_view(), name='poll_create'),
+    path('<int:pk>/edit',PollEdit.as_view(),name='poll_edit'),
+    path('<int:pid>/add',OptionCreate.as_View(),name="option_create")
 ]
--- default/views.py
+++ default/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import Poll,Option     #從models引用
-from django.views.generic import ListView,DetailView , RedirectView
-from django.urls import reverse
+from django.views.generic import ListView,DetailView , RedirectView,CreateView,UpdateView
+from django.urls import reverse, reverse_lazy
 
 # Create your views here.
 def poll_list(req):
@@ -29,3 +29,22 @@ class PollVote(RedirectView):
         #return f"/poll/{option.poll_id}"
         return reverse("poll_view",args=[option.poll_id])
         #return reverse("poll_view",kwargs=['pk':option.poll_id])
+class PollCreate(CreateView):
+    model = Poll
+    fields = "__all__"#["subject",'desc']
+    success_url =reverse_lazy('poll_list')
+class PollEdit(UpdateView):
+    model = Poll
+    fields = "__all__"#["subject",'desc']
+    def get_success_url(self):
+        return reverse_lazy('poll_view',kwargs={'pk':self.object.id})
+    #
+class OptionCreate(CreateView):
+    model = Option
+    fields = ['title']
+    def form_valid(self, form):
+        form.instance.poll_id = self.kwargs['pid']
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('poll_view',kwargs={'pk':self.kwargs['pid']})
